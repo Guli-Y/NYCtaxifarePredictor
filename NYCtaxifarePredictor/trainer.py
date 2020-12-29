358b11e8e89da5c0100573995ea48d58c1389373
Guli <g.yimingjiang@gmail.com>
2020-12-29 19:16:05 +0100

    added uploading trained model to gcp

--- NYCtaxifarePredictor/trainer.py
+++ NYCtaxifarePredictor/trainer.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from NYCtaxifarePredictor.data import get_data, clean_train
+from NYCtaxifarePredictor.data import get_data, clean_data
 from NYCtaxifarePredictor.transformer import TimeFeatures, DistanceFeatures, DirectionFeatures, AirportFeatures
 from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from scipy import stats
@@ -13,9 +13,16 @@ from mlflow.tracking import MlflowClient
 from memoized_property import memoized_property
 from termcolor import colored
 import time
-
+from google.cloud import storage
+import os
+import joblib
+################################# MLflow ###################################
 MLFLOW_URI = "https://mlflow.lewagon.co/"
 EXPERIMENT_NAME = '[DE][Berlin][Guli]NYCtaxifarePredictor'
+################################# GCP ######################################
+BUCKET_NAME = 'nyc_taxifare_predictor'
+MODEL_NAME = 'xgboost'
+VERSION_NAME = 'RunNo1'
 
 class Trainer():
     X = ['pickup_datetime',
@@ -25,17 +32,17 @@ class Trainer():
     def __init__(self, df, **kwargs):
         self.kwargs = kwargs
         self.mlflow = self.kwargs.get('mlflow', False)
-        self.estimator = self.kwargs.get('estimator', 'xgboost')
+        self.estimator = self.kwargs.get('estimator', MODEL_NAME)
         self.estimator_params = self.kwargs.get('estimator_params',
-                                                dict(learning_rate=[0.1],
+                                                dict(learning_rate=[0.12],
                                                     n_estimators=[100],
-                                                    max_depth=[5],
-                                                    min_child_weight=[2]))
+                                                    max_depth=[6],
+                                                    min_child_weight=[6]))
         self.run_name = self.kwargs.get('run_name', 'unknown')
         self.nrows = df.shape[0]
         print(colored('---------------------- loading data  ----------------------', 'green'))
         self.pipeline = None
-        self.test = None
+        self.val = None
         self.train_time = None
         self.split = self.kwargs.get("split", True)
         print(colored(f'---------------------- split = {self.split} ----------------------', 'red'))
@@ -45,13 +52,12 @@ class Trainer():
                                                 random_state=5))
             print(self.split_params)
             print(colored('---------------------- splitting train/val  ----------------------', 'blue'))
-            self.train, self.test = train_test_split(df, **self.split_params)
-            self.test = self.test.dropna()
+            self.train, self.val = train_test_split(df, **self.split_params)
+            self.val = self.val.dropna()
         else:
             self.train = df
         del df
-        print(colored('---------------------- cleaning data  ----------------------', 'blue'))
-        self.train = clean_train(self.train)
+        self.train = clean_data(self.train)
 
     def get_estimator(self):
         """return estimator"""
@@ -89,17 +95,19 @@ class Trainer():
         grid_params = \
                     {'model__'+key: val for key, val in self.estimator_params.items()}
         clf = GridSearchCV(self.pipeline, param_grid=grid_params, cv=5,
-                            scoring='neg_root_mean_squared_error', verbose=1)
+                            scoring='neg_root_mean_squared_error',
+                            return_train_score=True,
+                            verbose=1)
         print(colored('---------------------- start training  ----------------------', 'green'))
         start = time.time()
         self.pipeline = clf.fit(self.train[self.X], self.train[self.y])
         end = time.time()
         self.train_time = int(end-start)
         print('Grid Scores:')
-        means = self.pipeline.cv_results_['mean_test_score']
-        stds = self.pipeline.cv_results_['std_test_score']
-        for mean, std, params in zip(means, stds, self.pipeline.cv_results_['params']):
-            print(f'rmse = {-round(mean,3)} (+/-{round(std,3)}) for {params}')
+        means_test = self.pipeline.cv_results_['mean_test_score']
+        means_train = self.pipeline.cv_results_['mean_train_score']
+        for mean_test, mean_train, params in zip(means_test, means_train, self.pipeline.cv_results_['params']):
+            print(f'test:{-round(mean_test,3)} train:{-round(mean_train,3)} for {params}')
         print(colored('---------------------- best_params ----------------------', 'red'))
         print(self.pipeline.best_params_)
         print('train time:', self.train_time)
@@ -107,28 +115,32 @@ class Trainer():
     def get_rmse(self, df):
         if self.pipeline is None:
             print(colored("The model hasn't been trained yet", 'red'))
-        if self.test is None:
-            print(colored("Test data not available. Please consider set split parameter to True.", 'red'))
+        if self.val is None:
+            print(colored("Validation data not available. Please consider set split parameter to True.", 'red'))
         y_pred = self.pipeline.predict(df[self.X])
         y_true = df[self.y]
         return round(np.sqrt(np.mean((y_true-y_pred)**2)), 3)
 
     def evaluate(self):
         print(colored('---------------------- evaluating  ----------------------', 'green'))
-        test_rmse = self.get_rmse(self.test)
         train_rmse = self.get_rmse(self.train)
-        print(colored(f'---------------------- train_RMSE: {train_rmse}  ----------------------', 'red'))
-        print(colored(f'----------------------  test_RMSE: {test_rmse}  ----------------------', 'red'))
+        print(colored(f'----------------------  train_RMSE: {train_rmse}  ----------------------', 'red'))
+        if self.val is None:
+            val_rsme = 'not available'
+        else:
+            val_rmse = self.get_rmse(self.val)
+        print(colored(f'----------------------  val_RMSE: {val_rmse}  ----------------------', 'red'))
         if self.mlflow:
             print(colored('---------------------- logging params and metrics  ----------------------', 'green'))
             # logging params and metrics of each search in grid_search
-            means = self.pipeline.cv_results_['mean_test_score']
-            stds = self.pipeline.cv_results_['std_test_score']
-            for mean, std, params in zip(means, stds, self.pipeline.cv_results_['params']):
+            means_test = self.pipeline.cv_results_['mean_test_score']
+            means_train = self.pipeline.cv_results_['mean_train_score']
+            for mean_test, mean_train, params in zip(means_test, means_train, self.pipeline.cv_results_['params']):
                 with mlflow.start_run(experiment_id=self.mlflow_experiment_id, run_name=self.run_name+'_grid_search'):
                     for key, val in params.items():
                         mlflow.log_param(key[7:], val)
-                    mlflow.log_metric('cv_RMSE', -mean)
+                    mlflow.log_metric('test_RMSE', -mean_test)
+                    mlflow.log_metric('train_RMSE', -mean_train)
             # logging params and metrics of trainer
             with mlflow.start_run(experiment_id=self.mlflow_experiment_id, run_name=self.run_name):
                 mlflow.log_param('nrows', self.nrows)
@@ -139,8 +151,8 @@ class Trainer():
                 mlflow.log_param('grid_params', self.estimator_params)
                 for key, val in self.pipeline.best_params_.items():
                     mlflow.log_param('best' + key[5:], val)
+                mlflow.log_metric('val_RMSE', val_rmse)
                 mlflow.log_metric('train_RMSE', train_rmse)
-                mlflow.log_metric('test_RMSE', test_rmse)
                 mlflow.log_metric('train_time', self.train_time)
 
     def predict(self, test):
@@ -154,8 +166,19 @@ class Trainer():
     def save_model(self):
         if self.pipeline is None:
             raise ("No model hasn't been trained")
-        joblib.dump(self.pipeline, self.estimator+'.joblib')
-        print(colored("Model saved locally", 'green'))
+        # save trained model locally
+        joblib.dump(self.pipeline, 'model.joblib')
+        print(colored("-------------------- model saved locally --------------------", 'green'))
+        # upload trained model to GCP storage
+        client = storage.Client()
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(f'models/{MODEL_NAME}/{VERSION_NAME}/model.joblib')
+        blob.upload_from_filename('model.joblib')
+
+        location = f'gs://{BUCKET_NAME}/models/{MODEL_NAME}/{VERSION_NAME}/model.joblib'
+        print(colored(f'uploaded the trained model to GCP cloud storage \n => {location}', 'green'))
+        # delete locally saved model
+        os.remove('model.joblib')
 
     ############################# MLflow part #################################
     @memoized_property
@@ -173,7 +196,13 @@ class Trainer():
 
 
 if __name__=='__main__':
-    df = get_data(n=1000)
-    xgb = Trainer(df)
+    df = get_data(n=1000000)
+    xgb = Trainer(df, mlflow=True, run_name=VERSION_NAME, estimator_params=dict(
+                                                    n_estimators=[100],
+                                                    max_depth=[5, 6, 7],
+                                                    min_child_weight=[5, 6, 7],
+                                                    learning_rate=[0.08, 0.1, 0.12]
+                                                    ))
     xgb.train_model()
     xgb.evaluate()
+    xgb.save_model()
