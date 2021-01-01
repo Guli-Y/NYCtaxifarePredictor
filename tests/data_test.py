64f40e1eaa2d68bfca3113c914aee82e8505a496
Guli <g.yimingjiang@gmail.com>
2021-01-01 21:02:37 +0100

    changed package version

--- tests/data_test.py
+++ tests/data_test.py
@@ -4,12 +4,8 @@
 from NYCtaxifarePredictor.data import get_data, clean_data
 import pytest
 
-
-def test_get_data():
-    df = get_data(n=1000)
-    assert df.shape == (1000, 8)
-
 def test_clean_data():
     df = get_data(n=1000)
+    assert df.shape == (1000, 8)
     df_clean = clean_data(df)
     assert df_clean.shape == (976, 9)
