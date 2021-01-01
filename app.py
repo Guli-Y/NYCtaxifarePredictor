a854b651cb9b97e2a4c6592974e2debb86f666a2
Guli <g.yimingjiang@gmail.com>
2021-01-01 22:52:48 +0100

    added google credientials

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import joblib
 app = Flask(__name__)
 CORS(app)
 
-DEFAULT_PARAMS = {'pickup_latitude': 40.76244539554292,
-                    'pickup_longitude': -73.98518146444557,
+DEFAULT_PARAMS = {'pickup_latitude': 40.76244,
+                    'pickup_longitude': -73.98518,
                     'pickup_datetime': str(datetime.utcnow())+' UTC'}
 
 
@@ -28,7 +28,7 @@ def format_input(input):
 def index():
     return 'OK'
 
-PIPELINE = joblib.load('data/models_xgboost_RunNo4_model.joblib')
+PIPELINE = load_model()
 
 @app.route('/predict_fare', methods=['GET', 'POST'])
 def predict_fare():
@@ -40,14 +40,6 @@ def predict_fare():
     result = PIPELINE.predict(df)
     result = [round(float(fare), 3) for fare in result]
     return {'predictions': result}
-
-@app.route('/set_model', methods=['GET', 'POST'])
-def set_model():
-    inputs = request.get_json()
-    model_name = inputs['model_name']
-    model_version = inputs['model_version']
-    PIPELINE = load_model(model_name=model_name, version_name=model_version)
-    return {'response': f'successfully loaded {model_name} model ({model_version}) from gcp'}
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
