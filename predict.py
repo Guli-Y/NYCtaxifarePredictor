c8bb60a1b13e696f7dbd084b896506395d12dd60
Guli <g.yimingjiang@gmail.com>
2021-01-04 04:40:41 +0100

    changed kaggle message

--- predict.py
+++ predict.py
@@ -3,19 +3,19 @@ from NYCtaxifarePredictor.gcp import load_model
 from termcolor import colored
 import os
 
-KAGGLE_MESSAGE = 'RunNo'
+VERSION_NAME = 'RunNo8'
 
 def predict():
     test = get_test_data()
-    model = load_model()
-    test['fare_amount'] = model.predict(test)
     test.set_index('key', inplace=True)
+    model = load_model(version_name=VERSION_NAME)
+    test['fare_amount'] = model.predict(test)
     result = test[['fare_amount']]
     result.to_csv('prediction.csv')
     print(colored('------------------ prediction saved as csv file -----------------', 'green'))
 
 def kaggle_upload():
-    command = f'kaggle competitions submit -c new-york-city-taxi-fare-prediction -f "prediction.csv" -m "{KAGGLE_MESSAGE}"'
+    command = f'kaggle competitions submit -c new-york-city-taxi-fare-prediction -f "prediction.csv" -m "{VERSION_NAME}_trained with whole dataset"'
     os.system(command)
 
 if __name__ == '__main__':
