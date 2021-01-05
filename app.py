581708198e60adb59b314de29f7ba21dfba76bc4
Guli <g.yimingjiang@gmail.com>
2021-01-05 20:32:57 +0100

    using streamlit to deploy web app on heroku

--- app.py
+++ app.py
@@ -1,45 +1,58 @@
-from flask import Flask, request
-from flask_cors import CORS
+import streamlit as st
 from datetime import datetime
-from NYCtaxifarePredictor.gcp import load_model
 import pandas as pd
 import joblib
+import herepy
+import os
+from dotenv import load_dotenv
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+HERE_API_KEY = os.getenv('HERE_API_KEY')
 
-app = Flask(__name__)
-CORS(app)
+X = ['pickup_datetime',
+        'pickup_longitude',
+        'pickup_latitude',
+        'dropoff_longitude',
+        'dropoff_latitude']
 
-DEFAULT_PARAMS = {'pickup_latitude': 40.76244,
-                    'pickup_longitude': -73.98518,
-                    'pickup_datetime': str(datetime.utcnow())+' UTC'}
+st.markdown('https://github.com/Guli-Y/NYCtaxifarePredictor')
 
+def geocoder(address, key= HERE_API_KEY):
+    geoapi = herepy.GeocoderApi(api_key=key)
+    result = geoapi.free_form(address).as_dict()
+    coords = result["items"][0]["position"]
+    coords = {k.lower().replace('lng', 'lon'):v for k, v in coords.items()}
+    return coords
 
-def format_input(input):
-    for key, value in DEFAULT_PARAMS.items():
-        input.setdefault(key, value)
-    formated_input = {'pickup_datetime': str(input['pickup_datetime']),
-                        'pickup_latitude': float(input['pickup_latitude']),
-                        'pickup_longitude': float(input['pickup_longitude']),
-                        'dropoff_latitude': float(input['dropoff_latitude']),
-                        'dropoff_longitude': float(input['dropoff_longitude'])}
+def format_input(pickup, dropoff):
+    pickup_datetime = datetime.utcnow()
+    formated_input = {'pickup_datetime': str(pickup_datetime)+' UTC',
+                        'pickup_latitude': float(pickup['lat']),
+                        'pickup_longitude': float(pickup['lon']),
+                        'dropoff_latitude': float(dropoff['lat']),
+                        'dropoff_longitude': float(dropoff['lon'])}
     return formated_input
 
-@app.route('/')
-def index():
-    return 'OK'
-
-PIPELINE = load_model()
-
-@app.route('/predict_fare', methods=['GET', 'POST'])
-def predict_fare():
-    inputs = request.get_json()
-    if isinstance(inputs, dict):
-        inputs = [inputs]
-    inputs = [format_input(point) for point in inputs]
-    df = pd.DataFrame(inputs)
-    result = PIPELINE.predict(df)
-    result = [round(float(fare), 3) for fare in result]
-    return {'predictions': result}
+def main():
+    pipe = joblib.load('model.joblib')
+    print('------------ loaded model ---------------')
+    st.header('NYC Taxi Fare Predictor :taxi:')
+    st.write('Please type in pickup and dropoff locations to get predicted taxi fare amount!')
+    # input
+    pickup_address = st.text_input('pickup address', '45 Rockefeller Plaza, New York, NY 10111')
+    dropoff_address = st.text_input('dropoff address', '334 Furman St, Brooklyn, NY 11201')
+    # get coords
+    pickup_coords = geocoder(pickup_address)
+    dropoff_coords = geocoder(dropoff_address)
+    # input dictionary
+    df = pd.DataFrame([format_input(pickup_coords, dropoff_coords)])
+    df = df[X]
+    result = pipe.predict(df)
+    fare = round(float(result[0]), 3)
+    st.write(':oncoming_taxi: *Fare Amount*', fare, ':heavy_dollar_sign:')
+    locations = pd.DataFrame([pickup_coords, dropoff_coords])
+    st.map(data=locations, zoom=11)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    main()
